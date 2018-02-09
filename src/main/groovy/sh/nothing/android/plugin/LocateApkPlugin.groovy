4218ca61d5fe825a86f2e31db7a3a5efe1d4d0c8
shiraji <isogai.shiraji@gmail.com>
2018-02-09 18:52:21 +0900

    Remove double quotes

--- src/main/groovy/sh/nothing/android/plugin/LocateApkPlugin.groovy
+++ src/main/groovy/sh/nothing/android/plugin/LocateApkPlugin.groovy
@@ -8,7 +8,7 @@ class LocateApkPlugin implements Plugin<Project> {
     void apply(Project target) {
         target.android.applicationVariants.all { v ->
             v.assemble.doLast {
-                println "${v.outputs.first().outputFile}"
+                println v.outputs.first().outputFile
             }
             target.task("locate${v.name.capitalize()}") << {
                 ["open", "-R", v.outputs.first().outputFile].execute()
