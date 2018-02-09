15cb65a8a566aad58a0cf2ff5900edd91e83650b
tnj <kitakore@gmail.com>
2018-02-09 14:49:32 +0900

    initial commit

---
+++ src/main/groovy/sh/nothing/android/plugin/LocateApkPlugin.groovy
@@ -0,0 +1,22 @@
+package sh.nothing.android.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LocateApkPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project target) {
+        target.android.applicationVariants.all { v ->
+            v.assemble.doLast {
+                println "${v.outputs.first().outputFile}"
+            }
+            target.task("locate${v.name.capitalize()}") << {
+                ["open", "-R", v.outputs.first().outputFile].execute()
+            }
+            target.task("assembleAndLocate${v.name.capitalize()}", dependsOn: v.assemble) << {
+                ["open", "-R", v.outputs.first().outputFile].execute()
+            }
+        }
+    }
+}
+
